acb681699b27ed370c4d78d52cbe2ca399c9d381
Mervet_Magdy <mervetmagdy28@gmail.com>
2023-02-17 13:55:03 +0200

    add app bar to edit view

--- lib/widgets/notes_body.dart
+++ lib/widgets/notes_body.dart
@@ -6,6 +6,7 @@ class NotesBody extends StatelessWidget {
   const NotesBody({Key? key}) : super(key: key);
 
   @override
+
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 24, right: 24, top: 50),
