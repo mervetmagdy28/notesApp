56afcb6d3349aca169aab623834c65a830337378
Mervet_Magdy <mervetmagdy28@gmail.com>
2023-02-17 14:14:12 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title;
+  final String content;
+  final String date;
+
+  NoteModel({
+    required this.title,
+    required this.date,
+    required this.content,
+  });
+}
