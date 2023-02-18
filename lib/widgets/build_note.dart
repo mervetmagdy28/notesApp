24bcdb997f66d711746dc5b7734be6b71a50cb79
Mervet_Magdy <mervetmagdy28@gmail.com>
2023-02-18 15:47:48 +0200

    take input and validate data

--- lib/widgets/build_note.dart
+++ lib/widgets/build_note.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notesapp/constants.dart';
-
 import 'custom_button.dart';
 import 'custom_text_field.dart';
 class BuildNewNote extends StatelessWidget {
@@ -8,16 +6,54 @@ class BuildNewNote extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:const [
-        Spacer(flex: 1,),
-        CustomTextField(hint: "title"),
-        Spacer(flex: 1,),
-        CustomTextField(hint: "content", maxLines: 5,),
-        Spacer(flex: 8,),
-        CustomButton(text: 'Add',),
-        Spacer(flex: 1,),
-      ],
+    return const FormNote();
+  }
+}
+
+class FormNote extends StatefulWidget {
+  const FormNote({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<FormNote> createState() => _FormNoteState();
+}
+
+class _FormNoteState extends State<FormNote> {
+  GlobalKey<FormState> formKey= GlobalKey();
+  AutovalidateMode autoValidateMode=AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autoValidateMode,
+
+      child: Column(
+        children: [
+          const Spacer(flex: 1,),
+          CustomTextField(hint: "title",
+              onSaved: (value){
+            title=value;
+          }),
+          const Spacer(flex: 1,),
+          CustomTextField(hint: "content", maxLines: 5, onSaved: (value){
+            subTitle=value;
+          }),
+          const Spacer(flex: 8,),
+          CustomButton(text: 'Add',onTap: (){
+            if (formKey.currentState!.validate()){
+              formKey.currentState!.save();
+            }else{
+              autoValidateMode=AutovalidateMode.always;
+              setState(() {
+
+              });
+            }
+          },),
+          const Spacer(flex: 1,),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -4,21 +4,25 @@ import '../constants.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({
-    Key? key, required this.text,
+    Key? key, required this.text, this.onTap,
   }) : super(key: key);
 final String text;
+final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: Container(
-        width: double.infinity,
-        height: 40,
-        decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.circular(8),
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Container(
+          width: double.infinity,
+          height: 40,
+          decoration: BoxDecoration(
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Center(child: Text(text, style: const TextStyle(color: Colors.black),)),
         ),
-        child: Center(child: Text(text, style: const TextStyle(color: Colors.black),)),
       ),
     );
   }
